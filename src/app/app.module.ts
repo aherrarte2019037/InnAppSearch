eb34735cb9f4a6e23cebf2665eec414631ee820a
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-09-09 19:22:34 -0600

    Se crearon variables de entorno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
