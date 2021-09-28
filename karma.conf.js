0900ef4918b2b1b603850d0c0823ee68b4c5ab9c
Angel Herrarte <angelherrarte3@gmail.com>
2021-09-28 12:25:45 -0600

    Se agregó logo de aplicación

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/InnAppSearch'),
+      dir: require('path').join(__dirname, './coverage/Copher'),
       subdir: '.',
       reporters: [
         { type: 'html' },
